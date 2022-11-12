a79f73e5b0490cfb276a3f0c35c1c8ed89b8151a
Ghostop02 <115125956+Ghostop02@users.noreply.github.com>
2022-11-12 23:50:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -95,7 +95,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", 
+                    text=f"📂{get_size(file.file_size)} 🎥{file.file_name}", callback_data=f'files#{file.file_id}', 
                     url=await get_shortlink(f"https://t.me/{temp.U_NAME}?start=files_{file.file_id}")
                 ),
                 InlineKeyboardButton(
