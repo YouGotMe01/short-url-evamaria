3e82c940c20ebc8d745e1cf4eb4db4a321d76ad9
Ghostop02 <115125956+Ghostop02@users.noreply.github.com>
2023-01-12 23:53:16 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('🌐 𝖲𝗎𝗉𝗉𝗈𝗋𝗍 🌐', url="https://t.me/+2YaY1CQKOfg3MzQ9"),
-            InlineKeyboardButton('📢 𝖴𝗉𝖽𝖺𝗍𝖾𝗌', url='https://t.me/isaimini_updates')
+            InlineKeyboardButton('📢 𝖴𝗉𝖽𝖺𝗍𝖾𝗌', url='https://t.me/isaimini_predvd')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
