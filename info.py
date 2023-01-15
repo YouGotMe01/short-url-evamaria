ac3a0e8d8833ea9fb3a8909d42ddd363a0b7ae2d
Ghostop02 <115125956+Ghostop02@users.noreply.github.com>
2023-01-15 15:05:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://itsme:itsme@cluster0.b4hh3yz.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://evamaria:evamaria@cluster0.f91rn.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Evamaria")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
