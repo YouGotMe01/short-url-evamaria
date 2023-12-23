1342068a92ffe1dd1bc8394000d98786ce391db8
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2023-12-23 23:53:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,10 +23,10 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/3ae93577d725232d76c70.jpg h
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1392566136').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001870620638').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL', '-1001814606077')
+auth_channel = environ.get('AUTH_CHANNEL', '-1002017955864')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
