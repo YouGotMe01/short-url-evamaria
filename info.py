5c4ecb97a5bfd8fc9d14a8965416a1c044af499f
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2023-12-24 00:49:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,6 +15,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '18037664'))
 API_HASH = environ.get('API_HASH', '2f30344d1a5d5fefc42241ab6c65d02d')
 BOT_TOKEN = environ.get('BOT_TOKEN', "6131262953:AAEJ_i0pK74C3jsp49NQP8OkGIyDxdsw0_g")
+PORT = environ.get("PORT", "8080")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
