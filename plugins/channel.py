794188fe67a24378bede409854b659c0ef5669c0
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2024-03-04 23:55:37 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,14 +1,15 @@
 from pyrogram import Client, filters
-from info import CHANNELS
+from info import CHANNELS, UPDATES_CHNL
 from database.ia_filterdb import save_file
+from utils import add_chnl_message
 
-media_filter = filters.document | filters.video | filters.audio
+media_filter = filters.document | filters.video
 
 
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
 async def media(bot, message):
     """Media Handler"""
-    for file_type in ("document", "video", "audio"):
+    for file_type in ("document", "video"):
         media = getattr(message, file_type, None)
         if media is not None:
             break
@@ -17,4 +18,9 @@ async def media(bot, message):
 
     media.file_type = file_type
     media.caption = message.caption
-    await save_file(media)
+    text = await save_file(media)
+    if text is not None:
+        final = await add_chnl_message(text)
+        if final is not None:
+            cap = "#PreDvDUpdate:\n\n"
+            await bot.send_message(chat_id=UPDATES_CHNL, text=f"<b>{cap}<code>{final}</code>\n\nCopy & Paste In Group To Search\n---»<a href=https://t.me/isaimini_updates/110> ᴍᴏᴠɪᴇ sᴇᴀʀᴄʜɪɴɢ ɢʀᴏᴜᴘ ʟɪɴᴋs </a>«---</b>", disable_web_page_preview=True, parse_mode=enums.ParseMode.HTML)
