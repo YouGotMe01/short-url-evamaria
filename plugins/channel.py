31912ef69829df9931dbc93a7c8c5090a30984bc
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2024-06-24 00:15:44 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,10 +1,16 @@
+import asyncio
+import logging
 from pyrogram import Client, filters, enums
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import BadRequest
 from info import CHANNELS, UPDATES_CHNL
 from database.ia_filterdb import save_file
-from utils import add_chnl_message
+from utils import add_chnl_message, get_poster, temp
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 media_filter = filters.document | filters.video
-
 
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
 async def media(bot, message):
@@ -20,7 +26,21 @@ async def media(bot, message):
     media.caption = message.caption
     text = await save_file(media)
     if text is not None:
-        final = await add_chnl_message(text)
-        if final is not None:
-            cap = "#PreDvDUpdate:\n\n"
-            await bot.send_message(chat_id=UPDATES_CHNL, text=f"<b>{cap}<code>{final}</code>\n\nCopy & Paste In Group To Search\n---»<a href=https://t.me/isaimini_updates/110> ᴍᴏᴠɪᴇ sᴇᴀʀᴄʜɪɴɢ ɢʀᴏᴜᴘ ʟɪɴᴋs </a>«---</b>", disable_web_page_preview=True, parse_mode=enums.ParseMode.HTML)
+        mv_naam, year, languages = await add_chnl_message(text)
+        if mv_naam is not None:
+            languages_str = " ".join(languages) if languages else None
+            mv_naam = mv_naam.replace(".", " ")
+            if year.isdigit():
+                caption = f"<b>#PreDvDUpdate:\n\n🧿 <u>𝐍𝐚𝐦𝐞</u> : <code>{mv_naam}</code>\n📆 <u>𝐘𝐞𝐚𝐫</u> : {year}\n"
+            if languages_str:
+                caption += f"🎙️<u>𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞</u> : {languages_str}\n"
+            caption += "\nCopy & Paste In Group To Search\n---»<a href=https://t.me/isaimini_updates/110> ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ ʟɪɴᴋs </a>«---</b>"
+            await bot.send_message(
+                chat_id=UPDATES_CHNL,
+                text=caption,
+                disable_web_page_preview=True,
+                parse_mode=enums.ParseMode.HTML
+            )
+            logger.info(f'{mv_naam} {year} - Update Sent to Channel!')
+            await asyncio.sleep(5)
+            return
