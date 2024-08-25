d820a3ace43cf96a70ae1c4a45fdd341811ed2a1
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2024-08-26 01:29:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -700,7 +700,7 @@ async def auto_filter(client, msg, spoll=False):
             search = search.replace("-", " ")
             search = search.replace(":", "")
             search = search.replace(".", "")
-            files, offset, total_results = await get_search_results(message.chat.id ,search, offset=0, filter=True)
+            files, offset, total_results = await get_search_results(search, offset=0, filter=True)
             if not files:
                 if settings["spell_check"]:
                     return await advantage_spell_chok(msg)
